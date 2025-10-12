2bfb1448a209d0bdee48fc27fda10da4096dd9ce
Harsh Gajjar <harshgajjar2812@gmail.com>
2025-10-13 04:04:39 +0530

    chore(cv): update skills and experience descriptions for clarity and detail

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -102,11 +102,11 @@ const SKILLS = {
     },
     {
       name: "FastAPI",
-      url: "https://fastapi.tiangolo.com/"
+      url: "https://fastapi.tiangolo.com/",
     },
     {
       name: "Flask",
-      url: "https://flask.palletsprojects.com/"
+      url: "https://flask.palletsprojects.com/",
     },
     {
       name: "React",
@@ -202,7 +202,7 @@ const SKILLS = {
     },
     {
       name: "Docker",
-      url: "https://docs.docker.com/"
+      url: "https://docs.docker.com/",
     },
     {
       name: "Cloudinary",
@@ -299,7 +299,7 @@ const EDUCATION = [
 
 const POSITIONS = [
   {
-    id: "hmc-core",  // Add unique id
+    id: "hmc-core", // Add unique id
     title: "Core Member",
     organization: "Hostel Management Committee, DA-IICT",
     organizationUrl: null,
@@ -310,7 +310,7 @@ const POSITIONS = [
     events: null,
   },
   {
-    id: "gdg-core",  // Add unique id
+    id: "gdg-core", // Add unique id
     title: "Core Member",
     organization: "Google Developer Group on Campus, DA-IICT",
     organizationUrl: "https://dscdaiict.in/",
@@ -339,7 +339,7 @@ const EXPERIENCE = [
     duration: "Mar 2025 - Jun 2025",
     location: "San Francisco, California, United States · (Remote)",
     description:
-      "Independently led the design and deployment of scalable APIs and data pipelines, reducing API latency by 30% and supporting 1,500+ concurrent users. Spearheaded React modules for dashboards, global leaderboards, and personalized roadmaps, driving an 80% boost in user engagement.",
+      "Designed and deployed scalable APIs with Node.js, MongoDB, and AWS, reducing latency by 30% for 2,500+ users. Built React and Next.js dashboards, global leaderboards, and personalized roadmaps, increasing engagement by 80%. Mentored junior developers on Node.js and system design  practices while collaborating with cross-functional teams.",
   },
   {
     title: "Full Stack Developer (Freelance)",
@@ -348,7 +348,7 @@ const EXPERIENCE = [
     duration: "May 2025 - Jul 2025",
     location: "Gandhinagar, Gujarat, India",
     description:
-      "Built a full-stack machine translation evaluation platform enabling users to submit translations, compute BLEU and other NLP metrics and view real-time leaderboards. Engineered secure APIs, dynamic dashboards and managed complete deployment in collaboration with lab researchers.",
+      "Built a full-stack machine translation platform with React, Vite, and Supabase for BLEU and NLP metric evaluations. Developed secure REST APIs, dashboards, and cloud deployment using PostgreSQL and system design principles. Collaborated with IR researchers to align platform features with academic and research goals.",
   },
 ];
 
@@ -375,8 +375,7 @@ export default function About() {
             rounded-3xl border border-white/10 p-8 
             hover:border-white/20 transition-all duration-500 
             group relative overflow-hidden
-            animate-in slide-in-from-bottom"
-        >
+            animate-in slide-in-from-bottom">
           {/* Gradient overlay effect */}
           <div
             className="absolute inset-0 bg-gradient-to-r from-white/5 to-transparent 
@@ -397,14 +396,12 @@ export default function About() {
             <div className="space-y-2 sm:space-y-4">
               <h1
                 className="text-2xl md:text-3xl lg:text-5xl font-bold py-3 mb-2 text-center 
-                bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-500"
-              >
+                bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-500">
                 नमस्ते, I'm Harsh Gajjar
               </h1>
               <div
                 className="text-xl md:text-2xl lg:text-4xl font-bold text-center min-h-[40px] md:min-h-[48px] 
-                bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-500"
-              >
+                bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-500">
                 <TypeAnimation
                   sequence={[
                     "Software Developer",
@@ -425,8 +422,7 @@ export default function About() {
             <p
               className="text-base md:text-lg lg:text-xl text-white/80 leading-relaxed md:leading-loose 
               max-w-full sm:text-left px-2 sm:px-0
-              transform transition-all duration-300 group-hover:text-white/90"
-            >
+              transform transition-all duration-300 group-hover:text-white/90">
               A developer crafting web solutions with modern tech stacks and
               frameworks. Passionate about AI/ML, I work on prediction models
               and AI agents while constantly exploring new technologies to
@@ -442,8 +438,7 @@ export default function About() {
                   transition-all duration-300 hover:-translate-y-1
                   backdrop-blur-sm group/skill
                   text-sm sm:text-base
-                  flex items-center gap-2"
-              >
+                  flex items-center gap-2">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   shapeRendering="geometricPrecision"
@@ -452,8 +447,7 @@ export default function About() {
                   fillRule="evenodd"
                   clipRule="evenodd"
                   viewBox="0 0 417 512.238"
-                  className="w-4 h-4 sm:w-5 sm:h-5"
-                >
+                  className="w-4 h-4 sm:w-5 sm:h-5">
                   <path
                     fill="#ffffff"
                     fillRule="nonzero"
@@ -471,8 +465,7 @@ export default function About() {
                   transition-all duration-300 hover:-translate-y-1
                   backdrop-blur-sm
                   text-sm sm:text-base
-                  flex items-center gap-2"
-              >
+                  flex items-center gap-2">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
@@ -483,8 +476,7 @@ export default function About() {
                   strokeWidth="2"
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  className="sm:w-5 sm:h-5"
-                >
+                  className="sm:w-5 sm:h-5">
                   <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
                   <polyline points="7 10 12 15 17 10" />
                   <line x1="12" y1="15" x2="12" y2="3" />
@@ -514,8 +506,7 @@ export default function About() {
                             hover:bg-white/10 hover:border-white/20 
                             transition-all duration-300 hover:-translate-y-1
                             backdrop-blur-sm group/skill
-                            text-sm sm:text-base"
-                        >
+                            text-sm sm:text-base">
                           <span className="relative">
                             {skill.name}
                             <span
@@ -543,8 +534,7 @@ export default function About() {
                             hover:bg-white/10 hover:border-white/20 
                             transition-all duration-300 hover:-translate-y-1
                             backdrop-blur-sm group/skill
-                            text-sm sm:text-base"
-                        >
+                            text-sm sm:text-base">
                           <span className="relative">
                             {skill.name}
                             <span
@@ -572,8 +562,7 @@ export default function About() {
                             hover:bg-white/10 hover:border-white/20 
                             transition-all duration-300 hover:-translate-y-1
                             backdrop-blur-sm group/skill
-                            text-sm sm:text-base"
-                        >
+                            text-sm sm:text-base">
                           <span className="relative">
                             {skill.name}
                             <span
@@ -601,8 +590,7 @@ export default function About() {
                             hover:bg-white/10 hover:border-white/20 
                             transition-all duration-300 hover:-translate-y-1
                             backdrop-blur-sm group/skill
-                            text-sm sm:text-base"
-                        >
+                            text-sm sm:text-base">
                           <span className="relative">
                             {skill.name}
                             <span
@@ -630,8 +618,7 @@ export default function About() {
                             hover:bg-white/10 hover:border-white/20 
                             transition-all duration-300 hover:-translate-y-1
                             backdrop-blur-sm group/skill
-                            text-sm sm:text-base"
-                        >
+                            text-sm sm:text-base">
                           <span className="relative">
                             {skill.name}
                             <span
@@ -656,8 +643,7 @@ export default function About() {
                             hover:bg-white/10 hover:border-white/20 
                             transition-all duration-300 hover:-translate-y-1
                             backdrop-blur-sm group/skill
-                            text-sm sm:text-base"
-                        >
+                            text-sm sm:text-base">
                           <span className="relative">
                             {skill.name}
                             <span
@@ -682,8 +668,7 @@ export default function About() {
                             hover:bg-white/10 hover:border-white/20 
                             transition-all duration-300 hover:-translate-y-1
                             backdrop-blur-sm group/skill
-                            text-sm sm:text-base"
-                        >
+                            text-sm sm:text-base">
                           <span className="relative">
                             {skill.name}
                             <span
@@ -709,16 +694,14 @@ export default function About() {
                     key={exp.title}
                     className="backdrop-blur-sm bg-white/5 rounded-2xl border border-white/10 
                       p-4 sm:p-6 hover:bg-white/10 hover:border-white/20 
-                      transition-all duration-300 group/exp"
-                  >
+                      transition-all duration-300 group/exp">
                     <div className="flex flex-col sm:flex-row sm:justify-between sm:items-start gap-2 mb-2">
                       {exp.organizationUrl ? (
                         <Link
                           href={exp.organizationUrl}
                           target="_blank"
                           rel="noopener noreferrer"
-                          className="text-lg sm:text-xl font-semibold hover:text-white/80 transition-colors"
-                        >
+                          className="text-lg sm:text-xl font-semibold hover:text-white/80 transition-colors">
                           <h3>{exp.organization}</h3>
                         </Link>
                       ) : (
@@ -747,19 +730,17 @@ export default function About() {
               <div className="space-y-4 sm:space-y-6">
                 {POSITIONS.map((pos) => (
                   <div
-                    key={pos.id}  // Change from title to id
+                    key={pos.id} // Change from title to id
                     className="backdrop-blur-sm bg-white/5 rounded-2xl border border-white/10 
                     p-4 sm:p-6 hover:bg-white/10 hover:border-white/20 
-                    transition-all duration-300 group/edu"
-                  >
+                    transition-all duration-300 group/edu">
                     <div className="flex flex-col sm:flex-row sm:justify-between sm:items-start gap-2 mb-2">
                       {pos.organizationUrl ? (
                         <Link
                           href={pos.organizationUrl}
                           target="_blank"
                           rel="noopener noreferrer"
-                          className="text-lg sm:text-xl font-semibold hover:text-white/80 transition-colors"
-                        >
+                          className="text-lg sm:text-xl font-semibold hover:text-white/80 transition-colors">
                           <h3>{pos.organization}</h3>
                         </Link>
                       ) : (
@@ -786,8 +767,7 @@ export default function About() {
                             rel="noopener noreferrer"
                             className="px-2 sm:px-3 py-1 text-xs sm:text-sm bg-white/5 border border-white/10 
                               rounded-full group-hover/edu:bg-white/10 
-                              group-hover/edu:border-white/20 transition-all duration-300 hover:text-white/80"
-                          >
+                              group-hover/edu:border-white/20 transition-all duration-300 hover:text-white/80">
                             {event.name}
                           </Link>
                         ))}
@@ -808,15 +788,13 @@ export default function About() {
                     key={edu.school}
                     className="backdrop-blur-sm bg-white/5 rounded-2xl border border-white/10 
                       p-4 sm:p-6 hover:bg-white/10 hover:border-white/20 
-                      transition-all duration-300 group/edu rounded-2xl"
-                  >
+                      transition-all duration-300 group/edu rounded-2xl">
                     <div className="flex flex-col sm:flex-row sm:justify-between sm:items-start gap-2 mb-2">
                       <Link
                         href={edu.schoolUrl}
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="text-lg sm:text-xl font-semibold hover:text-white/80 transition-colors"
-                      >
+                        className="text-lg sm:text-xl font-semibold hover:text-white/80 transition-colors">
                         <h3>{edu.school}</h3>
                       </Link>
                       <span className="text-white/60 text-sm">
@@ -833,8 +811,7 @@ export default function About() {
                           key={course}
                           className="px-2 sm:px-3 py-1 text-xs sm:text-sm bg-white/5 border border-white/10 
                             rounded-full group-hover/edu:bg-white/10 
-                            group-hover/edu:border-white/20 transition-all duration-300"
-                        >
+                            group-hover/edu:border-white/20 transition-all duration-300">
                           {course}
                         </span>
                       ))}
