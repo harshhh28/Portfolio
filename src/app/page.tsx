107fff546379c7ef0c8e44ff3d54145ef05a3dbe
Harsh Gajjar <harshgajjar2812@gmail.com>
2026-05-03 01:49:34 +0530

    feat: initialize home page with SystemInfo component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import SystemInfo from "@/components/home/SystemInfo";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-background">
+    <div className="min-h-screen">
       <SystemInfo />
     </div>
   );
